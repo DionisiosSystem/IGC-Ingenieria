ec19fab7fe6a737343d45531beabc5488702c5fd
unknown <73070939+DionisiosSystem@users.noreply.github.com>
2020-11-24 21:08:21 -0300

    test unitarios

--- IGCIngenieria/IGCIngenieria/settings.py
+++ IGCIngenieria/IGCIngenieria/settings.py
@@ -129,7 +129,7 @@ AUTH_PASSWORD_VALIDATORS = [
 #LANGUAGE_CODE = 'en-us'
 LANGUAGE_CODE = 'es-eu'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Santiago'
 
 USE_I18N = True
 
--- IGCIngenieria/formulario/tests.py
+++ IGCIngenieria/formulario/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
 
 # Create your tests here.
+import json
+
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from django.utils import timezone
+
+
+
+class PruebaUnitarias(TestCase):   
+   
+    # POST api
+    def test_ingresarDatosAPI(self):
+        fecha_hora = timezone.now()
+        data= {"id":33,"Nombre":"Tablero Electrico","Valor":500000,
+        "Descripcion":"Tablero para empresa CumbreDental","Estado":True,
+        "Actualizacion" : fecha_hora} 
+        response = self.client.post("/api/ListaPrecio/", data)
+        self.assertEqual(response.status_code, 201)
+    # GET api 
+    def test_obtenerDatosAPI(self):      
+        response = self.client.get("/api/ListaPrecio/")
+        self.assertEqual(response.status_code, 200)
